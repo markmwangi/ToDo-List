c9d9b9685e2f28b0410ca966bee1b46d3f72e6e1
Mark Mwangi <35401129+markmwangi@users.noreply.github.com>
2019-03-09 16:42:56 -0700

    Added jQuery effects when adding and removing items.

--- JS/app.js
+++ JS/app.js
@@ -17,21 +17,27 @@ $(document).ready(function() {
         listItem = listItem.replace(/<(|\/|[^>\/bi]|\/[^>bi]|[^\/>][^>]+|\/[^>][^>]+)>/g, '');
         if(listItem.trim() != ''){
             let add = `<span class="items">${symbol}${listItem}</span>`;
+            add = $(add).hide();
             $('.container').append(add);
+            $('.container .items').show(400);
             $("#list-item").val('');
         }
     }
     
     //Remove the selected span on click
     $(document).on('click', '.items',  function(){
-        $(this).remove('.items');
+        let listItem = $(this);
+
+        $(listItem).hide(300, function(){
+            $(listItem).remove();
+            console.log('deleted');
+        });
     });
 
     //Updates list-items when hovered 
     $(document).on('mouseover', '.items', function(event){
         let item = $(event.target).get(0);
         let symbol = $(item).find('.symbol').get(0);
-
         $(item).hover(function(e){
             $(symbol).css('color', 'red');
             $(symbol).text('-');
