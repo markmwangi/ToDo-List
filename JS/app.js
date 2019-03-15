2f50be5ba4ece33eb810d93b0fb8f82f7e00344e
Mark Mwangi <35401129+markmwangi@users.noreply.github.com>
2019-03-14 19:31:13 -0700

    Added marked as complete feature. Improved functionality.

--- JS/app.js
+++ JS/app.js
@@ -1,49 +1,53 @@
 $(document).ready(function() {
-    let symbol = '<span class="symbol">></span>';
+  let check = '<input type="checkbox" class="check" />';
 
-    //Enter key listener
-    $('#list-item').on('keydown', function(event){
-        event.which == 13? update() : false;
-    });
+  //Enter key listener
+  $("#list-item").on("keydown", function(event) {
+    event.which == 13 ? update() : false;
+  });
 
-    //Button listener for the add button
-    $('#add-btn').click(function(){
-        update();
-    });
+  //Button listener for the add button
+  $("#add-btn").click(function() {
+    update();
+  });
 
-    //Validates input and adds it to container
-    function update(){
-        let listItem = $('#list-item').val();
-        listItem = listItem.replace(/<(|\/|[^>\/bi]|\/[^>bi]|[^\/>][^>]+|\/[^>][^>]+)>/g, '');
-        if(listItem.trim() != ''){
-            let add = `<span class="items">${symbol}${listItem}</span>`;
-            add = $(add).hide();
-            $('.container').append(add);
-            $('.container .items').show(400);
-            $("#list-item").val('');
-        }
+  //Validates input and adds it to container
+  function update() {
+    let listItem = $("#list-item").val();
+    listItem = listItem.replace(
+      /<(|\/|[^>\/bi]|\/[^>bi]|[^\/>][^>]+|\/[^>][^>]+)>/g,
+      ""
+    );
+    if (listItem.trim() != "") {
+      let add = `<span class="items">${check}${listItem}</span>`;
+      add = $(add).hide();
+      $(".container").append(add);
+      $(".container .items").show(400);
+      $("#list-item").val("");
     }
-    
-    //Remove the selected span on click
-    $(document).on('click', '.items',  function(){
-        let listItem = $(this);
+  }
 
-        $(listItem).hide(300, function(){
-            $(listItem).remove();
-            console.log('deleted');
-        });
+  //Removes checked off items from list
+  $("#clear-btn").click(function() {
+    console.log("working?");
+    $(".check:checked").each(function() {
+      item = $(this)
+        .parent()
+        .get(0);
+      $(item).hide(300, function() {
+        $(item).remove();
+      });
     });
+  });
 
-    //Updates list-items when hovered 
-    $(document).on('mouseover', '.items', function(event){
-        let item = $(event.target).get(0);
-        let symbol = $(item).find('.symbol').get(0);
-        $(item).hover(function(e){
-            $(symbol).css('color', 'red');
-            $(symbol).text('-');
-        }, function(){
-            $(symbol).css('color', 'black');
-            $(symbol).text('>');
-        });
-    });
+  //Changes symbol to delete symbol on hover
+  $(".container").on("click", ".check", function(event) {
+    parent = $(this).parent();
+    if (this.checked) {
+      console.log(parent);
+      parent.css("text-decoration", "line-through");
+    } else {
+      parent.css("text-decoration", "none");
+    }
+  });
 });
